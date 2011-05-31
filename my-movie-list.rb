2b4e8b6b081ff619785f0e03dc47564a6a69c883
Brian Dukes <dukes.brian@gmail.com>
2011-05-30 22:10:04 -0500

    Progress

--- my-movie-list.rb
+++ my-movie-list.rb
@@ -1,5 +1,19 @@
+require 'uri'
+require 'mongo'
+
+
+uri = URI.parse(ENV['MONGOHQ_URL'])
+conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
+db = conn.db(uri.path.gsub(/^\//, ''))
+api_key = db['keys'].find_one()
+movies = db['movies']
+
 class MyMovieList < Sinatra::Base
 	get '/' do
 		File.read(File.join('public', 'index.html'))	
+	end
+
+	post '/' do
+		movies.insert_movies_from_post
 	end
 end
